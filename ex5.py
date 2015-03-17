d172dbb7b845959e810f1e1c994e36ccffcc8684
MJHomar <michael.homar@gmail.com>
2015-03-16 17:11:24 -0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,31 +1,19 @@
-# this variable "cars" stores the total amount of cars
-cars = 100
+# here we are collecting data points
 
-# This variable "space_in_a_car" stores the Maximum seating allowed per car
-space_in_a_car = 4.0
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = "Brown"
 
-# This variable "drivers" stores the total amount of drivers
-drivers = 30
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His are usually %s  depending on the coffee." % my_teeth
 
-# This variable "passengers" is the total number of passengers
-passengers = 90
-
-# This variable "cars_not_driven" is the amount of cars not going to be used because there are not enough drivers.
-cars_not_driven = cars - drivers
-
-# This variable "cars_driven" is the amount of cars in use based on total drivers available
-cars_driven = drivers
-
-# This variable "carpool_capacity" is based on the total amount of cars to be driven (drivers) time the maximum seating of each car
-carpool_capacity = cars_driven * space_in_a_car
-
-# This variable "average_passengers_per_car" is determined by the total passengers by the total cars driven
-average_passengers_per_car = passengers / cars_driven
-
-# These print statements will print out the various text strings with the numbers in them.
-print "There are", cars, "cars avaiable."
-print "There are only", drivers, "drivers available."
-print "There will be", cars_not_driven, "empty cars today."
-print "WE can transport", carpool_capacity, "people today."
-print "We have", passengers, "passengers to carpool today."
-print "We need to put about", average_passengers_per_car, "in each car."
+# This line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
