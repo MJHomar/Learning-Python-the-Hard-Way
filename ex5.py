182c8ada1163bb726c797d15a55aa7e17f638b4d
MJHomar <michael.homar@gmail.com>
2015-03-16 21:23:16 -0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,19 +1,22 @@
-# here we are collecting data points
+# Here we are collecting data points
 
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = "Brown"
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+height_in_centimeters = (height * 2.45)
+weight = 180 # lbs
+weight_in_kilograms = weight / 2.2
+eyes = 'Blue'
+teeth = 'White'
+hair = "Brown"
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+# We are printing to the screen text strings and data from above
+print "Let's talk about %s." % name
+print "He's %d inches" % height, "or %d centimerters tall." % height_in_centimeters
+print "He's %d pounds" % weight, "or %d kilograms heavy." % weight_in_kilograms
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His are usually %s  depending on the coffee." % my_teeth
+print "He has %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee he drinks! :-)" % teeth
 
 # This line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
